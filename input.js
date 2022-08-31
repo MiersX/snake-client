8dd4930affd38fcb08d6d4a8989a2066d3bb81c7
MiersX <michael.muirhead123@gmail.com>
2022-08-31 23:44:34 +0000

    implemented connection object into function, and inputs into event handler

--- input.js
+++ input.js
@@ -1,4 +1,8 @@
-const setupInput = function() {
+let connection;
+
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -6,6 +10,22 @@ const setupInput = function() {
   const handleUserInput = function(key) {
     if (key === '\u0003') {
       process.exit();
+    }
+    // w Up
+    if (key === '\u0077') {
+      conn.write("Move: up");
+    }
+    // s Down
+    if (key === '\u0073') {
+      conn.write("Move: down");
+    }
+    // a Left
+    if (key === '\u0061') {
+      conn.write("Move: left");
+    }
+    // d Right
+    if (key === '\u0064') {
+      conn.write("Move: right");
     }
   };
   stdin.on('data', handleUserInput);
