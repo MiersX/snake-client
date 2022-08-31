4a63d175ad332696bb0677b1591d891ac3f76136
MiersX <michael.muirhead123@gmail.com>
2022-08-31 20:19:09 +0000

    Imported in the connect function exported from our client module

--- play.js
+++ play.js
@@ -1,23 +1,5 @@
 const net = require('net');
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-
-  conn.setEncoding('utf8');
-
-  conn.on("data", (message) => {
-    console.log(message);
-  });
-
-
-
-  return conn
-
-};
-
+const {connect} = require('./client');
 
 console.log('connecting ...');
 connect();
